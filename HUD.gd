801e223cfb94a69e8f6e7390ee917ec78d9bff0d
iver0 <jermacoff@gmail.com>
2023-04-02 22:02:19 +0700

    Initial commit

---
+++ HUD.gd
@@ -0,0 +1,36 @@
+extends CanvasLayer
+
+# Notifies `Main` node that the button has been pressed
+signal start_game
+
+
+func show_message(text):
+	$Message.text = text
+	$Message.show()
+	$MessageTimer.start()
+
+
+func show_game_over():
+	show_message("Game Over")
+	# Wait until the MessageTimer has counted down.
+	await $MessageTimer.timeout
+
+	$Message.text = "Dodge the\nCreeps!"
+	$Message.show()
+	# Make a one-shot timer and wait for it to finish.
+	await get_tree().create_timer(1.0).timeout
+	$StartButton.show()
+
+
+func update_score(score):
+	$ScoreLabel.text = str(score)
+
+
+func _on_StartButton_pressed():
+	$StartButton.hide()
+	$StartButton.hidden()
+	start_game.emit()
+
+
+func _on_MessageTimer_timeout():
+	$Message.hide()
---
+++ README.md
@@ -0,0 +1,2 @@
+# Dodge The Creeps
+A tutorial Godot 4 game.
